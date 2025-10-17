c758f5192bfefd8b180e2ee27c7d85461d0b6d47
Annelie Grape <annelie.grape@outlook.com>
2025-10-17 15:06:27 +0200

    Jag gjorde en ny klass i developer branchen. Testar att comitta och pusha som vanligt. (ej pull request)

---
+++ GitTest/AnnelieTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest
+{
+    public class AnnelieTest
+    {
+
+        public AnnelieTest()
+        {
+        }
+
+        public void TestMethod()
+        {
+            Console.WriteLine("Hej kompisar! Annelie här!");
+        }
+    }
+}
