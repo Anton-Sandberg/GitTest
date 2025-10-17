91fe2181bbe85e56a643a642cc91524bd0d74fde
Annelie Grape <annelie.grape@outlook.com>
2025-10-17 15:19:36 +0200

    min branch vanlig push

--- GitTest/AnnelieTest.cs
+++ GitTest/AnnelieTest.cs
@@ -8,7 +8,7 @@ namespace GitTest
 {
     public class AnnelieTest
     {
-        public string Name { get; set; } = "Annelie";
+        public string Name { get; set; } = "Annelies";
 
         public AnnelieTest()
         {
