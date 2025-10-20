c10282c61a0f5b680cb4f82041db3bca67bc606b
HenkeCoding <henrik.valentin@student.kyh.se>
2025-10-20 11:01:30 +0200

    Added Henkes cool class

---
+++ GitTest/HenkesCoolClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest;
+
+public class HenkesCoolClass
+{
+    public string CoolString { get; set; }
+
+    public HenkesCoolClass()
+    {
+        CoolString = "Henke is cool";
+    }
+
+    public void CoolMethod()
+    {
+        Console.WriteLine(CoolString);
+    }
+}
